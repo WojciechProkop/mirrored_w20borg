ceb59d07f83ed83f8983f50d51c17013bfeb562a
Wojciech Prokop <prokopw@mymacewan.ca>
2020-02-03 22:47:14 -0700

    Fix flipping functionality. Add cards flipping back when no match is made

--- scripts.js
+++ scripts.js
@@ -1,21 +1,48 @@
 
 //scripts.js
 
-//Find all .memCard objects
-const cards = document.querySelectorAll(".memCard");
-
+const deck = document.querySelectorAll(".memCard");
+let prevCard = null;
+let flips = 0;
 function flipCard()
 {
+    flips += 1;
     this.classList.toggle('flip');
+    // If this is not the first click
+    if(prevCard != null)
+    {   // If the this card and the previous card share the same name remove the event
+        // listener functionality, disabling the card face up. 
+        if(this.dataset.name === prevCard.dataset.name && prevCard != this)
+        {
+            this.removeEventListener('click', flipCard);
+            prevCard.removeEventListener('click', flipCard);
+            console.log("removed listeners");
+            prevCard = null
+            flips = 0;
+        }
+    }
+
+    console.log(flips);
+    // If you flipped two cards with no match, then flip them back down.
+    if(flips >= 2)
+    {
+        // Without this delay, last card "flipped" flips back too fast to 
+        // be seen.
+        setTimeout(() =>{
+        this.classList.toggle('flip');
+        prevCard.classList.toggle('flip');
+        prevCard = null;
+        flips = 0;
+    }, 1500);
+
+    }
+    else {prevCard = this}
 }
-
-console.log(cards[0]);// comes up as undefined, even though it should find
-                      // the memory cards. You have to use the electron
-                      // terminal to see the output.
-                      // Visual Studio Code doesn't print anything
+// Add eventListener events to every card during initialization, and call flipCard() when clicked.
+deck.forEach(c =>c.addEventListener('click', flipCard));
 
 
-// for some reason cards is empty, so forEach never executes
-cards.forEach(c => c.addEventListener('click', flipCard));
+
+
 
 
