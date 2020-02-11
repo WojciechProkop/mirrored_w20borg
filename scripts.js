3a1f0dbd58324612df10a46746e3e1da7ac0f0a3
estudillom <43586520+estudillom@users.noreply.github.com>
2020-02-10 22:12:07 -0700

    made changes

--- scripts.js
+++ scripts.js
@@ -4,8 +4,23 @@
 const deck = document.querySelectorAll(".memCard");
 let prevCard = null;
 let flips = 0;
+let blocked = false; // If false, user can flip. If true, user is locked out from playing
+randomize();
+
+//count matches
+let matchesMade = 0;
+//account for first flip
+let numMoves = 0;
+
+
 function flipCard()
 {
+    if(blocked)return;
+    //starts timer on first flip
+    if (numMoves === 0){
+        startTimer();
+    };
+    numMoves++;
     flips += 1;
     this.classList.toggle('flip');
     // If this is not the first click
@@ -17,6 +32,7 @@ function flipCard()
             this.removeEventListener('click', flipCard);
             prevCard.removeEventListener('click', flipCard);
             console.log("removed listeners");
+            matchesMade++;
             prevCard = null
             flips = 0;
         }
@@ -26,23 +42,51 @@ function flipCard()
     // If you flipped two cards with no match, then flip them back down.
     if(flips >= 2)
     {
+        blocked = true;
         // Without this delay, last card "flipped" flips back too fast to 
         // be seen.
+        if(prevCard != this)
+        {
         setTimeout(() =>{
         this.classList.toggle('flip');
         prevCard.classList.toggle('flip');
         prevCard = null;
-        flips = 0;
-    }, 1500);
-
+        blocked = false;
+            }, 1500);
+        }
+        setTimeout(()=>{
+            blocked = false;
+            flips = 0;
+            prevCard = null;
+        }, 1500);
+        
     }
     else {prevCard = this}
 }
+
+// Shuffle the board
+function randomize()
+{
+    deck.forEach(c => {
+        let ranPos = Math.floor(Math.random() * 12);
+        c.style.order = ranPos;
+    })
+}
+
+//timer
+function startTimer()
+{
+    var sec = 0, min = 0;
+    var Countup = setInterval(function(){
+        ++sec;
+        document.getElementById("timer").innerHTML = min+" min "+sec+" sec";
+        //if all cards match
+        if (sec == 60){
+            min++;
+            sec = 0;
+        }
+    },1000)
+}
+
 // Add eventListener events to every card during initialization, and call flipCard() when clicked.
 deck.forEach(c =>c.addEventListener('click', flipCard));
-
-
-
-
-
-
