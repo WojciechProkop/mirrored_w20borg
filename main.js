8eacec0353827b3f27520ba4e3ac8fc7b375a498
estudillom <43586520+estudillom@users.noreply.github.com>
2020-02-11 01:09:44 -0500

    undo file changes from estudillom

---
+++ main.js
@@ -0,0 +1,92 @@
+//C:/Electron folder
+
+const electron = require('electron');
+const url  = require('url');
+const path = require('path');
+
+const {app, BrowserWindow, Menu} = electron;
+
+// Storage for users score
+const storage = require('electron-json-storage');
+const defaultDataPath = storage.getDefaultDataPath()
+
+storage.set('Ashley',{score: 99}, function(error) {
+    if (error) throw error;
+});
+
+storage.set('Myron',{score: 55}, function(error) {
+    if (error) throw error;
+});
+
+storage.getAll(function(error, data) {
+    if (error) throw error;
+
+    console.log(data);
+});
+
+let mainWindow;
+
+// Listen for app to be ready
+app.on('ready', function () {
+
+    // Create new window
+    mainWindow = new BrowserWindow({});
+    //load html into window
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // Build Menu from Template
+    const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+    //Insert Menu
+    Menu.setApplicationMenu(mainMenu);
+});
+
+//Create menu Template
+
+const mainMenuTemplate = [
+    {
+        label: 'File',
+        submenu:[
+            {
+                label: 'Start',
+                click(){
+                    //ON Click on STart start game
+                }
+            },
+            {
+                label: 'Restart'
+            },
+            {
+                label: 'Quit',
+                accelerator: process.platform == 'darwin' ? 'Command+Q' :
+                    'Ctrl+Q',
+                click(){
+                    app.quit();
+                }
+            }
+        ]
+    }
+];
+
+// Add developer tools item if not in production
+if(process.env.NODE_ENV !== 'production'){
+    mainMenuTemplate.push({
+        label: 'Developer Tools',
+        submenu: [
+            {
+                label: 'Toggle DevTools',
+                accelerator: process.platform == 'darwin' ? 'Command+I' :
+                    'Ctrl+I',
+                click(item, focusedWindow){
+                    focusedWindow.toggleDevTools();
+                }
+            },
+            {
+                role: 'reload'
+            }
+        ]
+    })
+}
---
+++ scripts.js
@@ -0,0 +1,131 @@
+
+//scripts.js
+
+const deck = document.querySelectorAll(".memCard");
+let prevCard = null;
+let flips = 0;
+
+
+//for move counter
+let moves = 0;
+let counter = document.querySelector(".moves");
+let blocked = false; // If false, user can flip. If true, user is locked out from playing
+randomize();
+
+
+function flipCard()
+{
+    if(blocked)return;
+    flips += 1;
+    this.classList.toggle('flip');
+    // If this is not the first click
+    if(prevCard != null)
+    {   // If the this card and the previous card share the same name remove the event
+        // listener functionality, disabling the card face up. 
+        if(this.dataset.name === prevCard.dataset.name && prevCard != this)
+        {
+            this.removeEventListener('click', flipCard);
+            prevCard.removeEventListener('click', flipCard);
+            console.log("removed listeners");
+            prevCard = null
+            flips = 0;
+        }
+    }
+
+    console.log(flips);
+    // If you flipped two cards with no match, then flip them back down.
+    if(flips >= 2)
+    {
+        blocked = true;
+        // Without this delay, last card "flipped" flips back too fast to 
+        // be seen.
+        if(prevCard != this)
+        {
+        setTimeout(() =>{
+        this.classList.toggle('flip');
+        prevCard.classList.toggle('flip');
+        prevCard = null;
+        blocked = false;
+            }, 1500);
+        }
+        setTimeout(()=>{
+            blocked = false;
+            flips = 0;
+            prevCard = null;
+        }, 1500);
+        
+    }
+    else {prevCard = this}
+}
+
+// Shuffle the board
+function randomize()
+{
+    deck.forEach(c => {
+        let ranPos = Math.floor(Math.random() * 12);
+        c.style.order = ranPos;
+    })
+}
+
+// Add eventListener events to every card during initialization, and call flipCard() when clicked.
+deck.forEach(c =>c.addEventListener('click', flipCard));
+
+//Count the Players moves
+function moveCounter(){
+    moves++;
+    counter.innerHTML = moves;
+    //start timer on first click
+    if(moves == 1){
+        second = 0;
+        minute = 0;
+        hour = 0;
+        //startTimer(); insert timer here
+    }
+}
+
+const nameList = [
+    'Time', 'Past', 'Future', 'Dev',
+    'Fly', 'Flying', 'Soar', 'Soaring', 'Power', 'Falling',
+    'Fall', 'Jump', 'Cliff', 'Mountain', 'Rend', 'Red', 'Blue',
+    'Green', 'Yellow', 'Gold', 'Demon', 'Demonic', 'Panda', 'Cat',
+    'Kitty', 'Kitten', 'Zero', 'Memory', 'Trooper', 'XX', 'Bandit',
+    'Fear', 'Light', 'Glow', 'Tread', 'Deep', 'Deeper', 'Deepest',
+    'Mine', 'Your', 'Worst', 'Enemy', 'Hostile', 'Force', 'Video',
+    'Game', 'Donkey', 'Mule', 'Colt', 'Cult', 'Cultist', 'Magnum',
+    'Gun', 'Assault', 'Recon', 'Trap', 'Trapper', 'Redeem', 'Code',
+    'Script', 'Writer', 'Near', 'Close', 'Open', 'Cube', 'Circle',
+    'Geo', 'Genome', 'Germ', 'Spaz', 'Shot', 'Echo', 'Beta', 'Alpha',
+    'Gamma', 'Omega', 'Seal', 'Squid', 'Money', 'Cash', 'Lord', 'King',
+    'Duke', 'Rest', 'Fire', 'Flame', 'Morrow', 'Break', 'Breaker', 'Numb',
+    'Ice', 'Cold', 'Rotten', 'Sick', 'Sickly', 'Janitor', 'Camel', 'Rooster',
+    'Sand', 'Desert', 'Dessert', 'Hurdle', 'Racer', 'Eraser', 'Erase', 'Big',
+    'Small', 'Short', 'Tall', 'Sith', 'Bounty', 'Hunter', 'Cracked', 'Broken',
+    'Sad', 'Happy', 'Joy', 'Joyful', 'Crimson', 'Destiny', 'Deceit', 'Lies',
+    'Lie', 'Honest', 'Destined', 'Bloxxer', 'Hawk', 'Eagle', 'Hawker', 'Walker',
+    'Zombie', 'Sarge', 'Capt', 'Captain', 'Punch', 'One', 'Two', 'Uno', 'Slice',
+    'Slash', 'Melt', 'Melted', 'Melting', 'Fell', 'Wolf', 'Hound',
+    'Legacy', 'Sharp', 'Dead', 'Mew', 'Chuckle', 'Bubba', 'Bubble', 'Sandwich', 'Smasher', 'Extreme', 'Multi',
+    'Universe', 'Ultimate', 'Death', 'Ready', 'Monkey', 'Elevator', 'Wrench', 'Grease', 'Head', 'Theme',
+    'Grand', 'Cool', 'Kid', 'Boy', 'Girl', 'Vortex', 'Paradox'
+];
+
+let finalName = "";
+
+
+function generate(){
+    document.getElementById("nameplace").innerHTML = randName();
+}
+
+function randName(){
+    finalName = nameList[Math.floor( Math.random() * nameList.length )];
+    finalName += nameList[Math.floor( Math.random() * nameList.length )];
+    if ( Math.random() > 0.5 ) {
+        finalName += nameList[Math.floor( Math.random() * nameList.length )];
+    }
+    return finalName;
+};
+
+
+
+
+
