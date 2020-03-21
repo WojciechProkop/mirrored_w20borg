aa484f6a71cab9bb2ff44a0aa06a819b9d47ffc1
labuicka <labuicka@mymacewan.ca>
2020-03-20 22:31:28 -0600

    News Js file for storing leaderboard

---
+++ window.js
@@ -0,0 +1,132 @@
+function close_window() {
+    if (confirm("Ready to close the tutorial window and Start the game?")) {
+        close();
+    }
+}
+
+const nameList = [
+    'Time', 'Past', 'Future', 'Dev',
+    'Fly', 'Flying', 'Soar', 'Soaring', 'Power', 'Falling',
+    'Fall', 'Jump', 'Cliff', 'Mountain', 'Rend', 'Red', 'Blue',
+    'Green', 'Yellow', 'Gold', 'Demon', 'Demonic', 'Panda', 'Cat',
+    'Kitty', 'Kitten', 'Zero', 'Memory', 'Trooper', 'XX', 'Bandit',
+    'Fear', 'Light', 'Glow', 'Tread', 'Deep', 'Deeper', 'Deepest',
+    'Mine', 'Your', 'Worst', 'Enemy', 'Hostile', 'Force', 'Video',
+    'Game', 'Donkey', 'Mule', 'Colt', 'Cult', 'Cultist', 'Magnum',
+    'Assault', 'Recon', 'Trap', 'Trapper', 'Redeem', 'Code',
+    'Script', 'Writer', 'Near', 'Close', 'Open', 'Cube', 'Circle',
+    'Geo', 'Genome', 'Germ', 'Spaz', 'Shot', 'Echo', 'Beta', 'Alpha',
+    'Gamma', 'Omega', 'Seal', 'Squid', 'Money', 'Cash', 'Lord', 'King',
+    'Duke', 'Rest', 'Fire', 'Flame', 'Morrow', 'Break', 'Breaker', 'Numb',
+    'Ice', 'Cold', 'Rotten', 'Sick', 'Sickly', 'Janitor', 'Camel', 'Rooster',
+    'Sand', 'Desert', 'Dessert', 'Hurdle', 'Racer', 'Eraser', 'Erase', 'Big',
+    'Small', 'Short', 'Tall', 'Sith', 'Bounty', 'Hunter', 'Cracked', 'Broken',
+    'Sad', 'Happy', 'Joy', 'Joyful', 'Crimson', 'Destiny', 'Deceit', 'Lies',
+    'Lie', 'Honest', 'Destined', 'Bloxxer', 'Hawk', 'Eagle', 'Hawker', 'Walker',
+    'Zombie', 'Sarge', 'Capt', 'Captain', 'Punch', 'One', 'Two', 'Uno', 'Slice',
+    'Slash', 'Melt', 'Melted', 'Melting', 'Fell', 'Wolf', 'Hound',
+    'Legacy', 'Sharp', 'Dead', 'Mew', 'Chuckle', 'Bubba', 'Bubble', 'Sandwich', 'Smasher', 'Extreme', 'Multi',
+    'Universe', 'Ultimate', 'Death', 'Ready', 'Monkey', 'Elevator', 'Wrench', 'Grease', 'Head', 'Theme',
+    'Grand', 'Cool', 'Kid', 'Boy', 'Girl', 'Vortex', 'Paradox'
+];
+
+let finalName = "";
+
+function generate(){
+    document.getElementById("nameplace").innerHTML = randName();
+}
+
+function randName(){
+    finalName = nameList[Math.floor( Math.random() * nameList.length )];
+    finalName += nameList[Math.floor( Math.random() * nameList.length )];
+    if ( Math.random() > 0.5 ) {
+        finalName += nameList[Math.floor( Math.random() * nameList.length )];
+    }
+    return finalName;
+};
+
+
+
+
+function acceptname() {
+    // Once the user accepts the username save it in the file
+    let userName = document.getElementById("nameplace").innerHTML;
+
+    document.getElementById("username").innerHTML = document.getElementById("nameplace").innerHTML;
+    document.getElementById('user-button').disabled = true;
+    document.getElementById('gen-button').disabled = true;
+
+    const storage = require('electron-json-storage');
+    storage.set('Username', { mode: 'playermode', score:'score' }, function(error) {
+        if (error) throw error;
+    });
+
+
+    if(document.getElementById('single').checked){
+        // Store the username in the console with a score of zero
+        storage.set(userName, { mode: 'sinlge', score:null }, function(error) {
+            if (error) throw error;
+        });
+    }
+    if(document.getElementById('multi').checked){
+        // Store the username in the console with a score of zero
+        storage.set(userName, { mode: 'multi', score:null }, function(error) {
+            if (error) throw error;
+        });
+    }
+
+    storage.getAll(function(error, data) {
+        if (error) throw error;
+
+        console.log(data);
+    });
+
+}
+
+
+function saveScore() {
+    const query = location.search.substring(1);
+    const keyValues = query.split(/&/);
+    const keyValuePairs = keyValues[0].split(/=/);
+    const valuepairs = keyValuePairs[1].split(/%/);
+    let value = valuepairs[2];
+
+    document.getElementById("score").innerHTML = value;
+
+
+// Once the user accepts the username save it in the file
+    const storage = require('electron-json-storage');
+
+    storage.keys(function(error, keys) {
+        if (error) throw error;
+
+        for (let key of keys) {
+            console.log('There is a key called: ' + key);
+            storage.get(key, function(error, data) {
+                if (error) throw error;
+
+                console.log(data);
+                if(data.score == null){
+                    let gmmode = data.mode;
+                    storage.remove(key, function(error) {
+                        if (error) throw error;
+                    });
+                    storage.set(key, { mode: gmmode, score:value }, function(error) {
+                        if (error) throw error;
+                    });
+                }
+            });
+        }
+    });
+
+    storage.getAll(function(error, data) {
+        if (error) throw error;
+
+        console.log(data);
+    });
+
+
+
+
+}
+
